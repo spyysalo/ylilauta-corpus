b2b4aca7555c3031fcbdfbea38b054b28115f3e0
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-10-16 11:13:28 +0300

    Initial import

---
+++ scripts/ftexperiments.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# fastText experiments
+
+# https://stackoverflow.com/a/246128
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+set -euo pipefail
+
+REPETITIONS=10
+
+FASTTEXT="$HOME/git_checkout/fastText/fasttext"
+
+DATADIR="$SCRIPTDIR/../data"
+
+WORDVECDIR="$SCRIPTDIR/../wordvecs"
+
+TESTSET="$DATADIR/ylilauta-dev.txt"
+
+declare -a TRAINSETS=(
+    "$DATADIR/ylilauta-train-100.txt"
+    "$DATADIR/ylilauta-train-1000.txt"
+    "$DATADIR/ylilauta-train.txt"
+)
+
+declare -a PARAMS=(
+    ""
+    "-minn 3 -maxn 5 -epoch 25"
+)
+
+wordvecs=$(find "$WORDVECDIR" -name '*.vec' -o -name '*.bin')
+echo "Wordvecs found in $WORDVECDIR: $wordvecs" >&2
+
+for i in $(seq $REPETITIONS); do
+    for params in "${PARAMS[@]}"; do
+	for wv in "" $wordvecs; do
+	    if [ -z "$wv" ]; then
+		wvparams=""   # no pretrained wordvecs
+		shortwv=""
+	    else
+		dim=$(head -n 1 "$wv" | cut -d ' ' -f 2)
+		wvparams="-pretrainedVectors $wv -dim $dim"
+		shortwv="-pretrainedVectors $(basename $wv) -dim $dim"
+	    fi
+	    for trainset in "${TRAINSETS[@]}"; do
+		model=$(mktemp)
+		train=$(basename "$trainset")
+		echo "Train on $train, params $params $shortwv (round $i)" >&2
+		$FASTTEXT supervised \
+			  -verbose 1 \
+			  $params \
+			  $wvparams \
+			  -input "$trainset" \
+			  -output "$model"
+		test=$(basename "$TESTSET")
+		echo "Test on $test, trained $train, params $params $shortwv"\
+		     " (round $i)" >&2
+		p=$($FASTTEXT test "$model.bin" "$TESTSET" \
+			   | egrep 'P@1' | cut -f 2)
+		echo "$train"$'\t'"$params"$'\t'"$shortwv"$'\t'"$p"
+		rm -rf "$model.bin" "$model.vec"
+	    done
+	done
+    done
+done
